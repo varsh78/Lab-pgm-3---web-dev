d0300fc5495cb07e7dbafd81cb7e6abcabcc3b0f
Sri Varshini <85393357+varsh78@users.noreply.github.com>
2021-10-30 11:57:01 +0530

    Update lab pgm 3 - web dev.js

--- lab pgm 3 - web dev.js
+++ lab pgm 3 - web dev.js
@@ -1,4 +1,5 @@
-//Write a JavaScript code that displays text “TEXT-GROWING” with increasing font size in the interval of 100ms in RED COLOR, when the font size reaches 50pt it displays “TEXTSHRINKING” in BLUE color. Then the font size decreases to 5pt. 
+//Write a JavaScript code that displays text “TEXT-GROWING” with increasing font size in the interval of 100ms in RED COLOR, 
+//when the font size reaches 50pt it displays “TEXTSHRINKING” in BLUE color. Then the font size decreases to 5pt. 
 <html lang="en"> 
 <head> 
 <meta charset="UTF-8"> 
